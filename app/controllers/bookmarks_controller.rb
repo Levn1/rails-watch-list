aa8147c4460945c91e9589b336b7b6dac4381ee4
Noam Lev <levno.ro@gmail.com>
2021-08-19 16:54:27 +0200

    Progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def bookmark_params
+  params.require(:list).permit(:name)
+end
